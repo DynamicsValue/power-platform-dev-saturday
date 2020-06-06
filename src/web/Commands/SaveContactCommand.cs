4faa5ed9a9efa1860eb88023d0b89ab3ffeda3fe
jordimontana82 <jordi.montana@gmail.com>
2020-06-06 11:38:34 +0200

    Remove logic to code command logic from scratch

--- src/web/Commands/SaveContactCommand.cs
+++ src/web/Commands/SaveContactCommand.cs
@@ -8,35 +8,14 @@ namespace web.Commands
 {
     public class SaveContactCommand
     {
-        private const string Email = "emailaddress1";
-        private const string Contact = "contact";
-
         private readonly IOrganizationService _service;
-        public SaveContactCommand(IOrganizationService service)
+        public SaveContactCommand(IOrganizationService service) 
         {
             _service = service;
         }
-
         public GenericResult Save(ContactDetailsModel model)
         {
-            using(var ctx = new OrganizationServiceContext(_service))
-            {
-                var existingContact = (from c in ctx.CreateQuery(Contact)
-                                        where (string) c[Email] == model.Email
-                                        select c).FirstOrDefault();
-
-                if(existingContact == null)
-                {
-                    _service.Create(model.ToEntity());
-                    return GenericResult.Succeed();
-                }
-                else 
-                {
-                    return GenericResult.FailWith($"Contact with email '{model.Email}' already exists");
-                }
-            }
-            
-            
+            return null;
         }
 
     }
